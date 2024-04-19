18ddc6d5c99277318272da6771b8a926238e7438
gak25 <gak25@users.noreply.github.com>
2024-04-18 18:00:56 -0700

    Update tt06-gak25-8bit-cpu-ext.v
    Just adding multiplication and division instructions to the ISA. This is my first foray into computer architecture so just the smallest extension on Ramyad's work to learn the workflow.

--- src/tt06-gak25-8bit-cpu-ext.v
+++ src/tt06-gak25-8bit-cpu-ext.v
@@ -6,6 +6,9 @@
 `define ALU_SUB  3'b100
 `define ALU_XOR  3'b101
 `define ALU_INC  3'b110
+`define ALU_MUL 3'b111 // Adding multiplication opcode
+`define ALU_DIV 3'b110 // Adding division opcode (redefining existing increment if unused)
+
 
 // ISA --------------------------------------------------------------
 //-- R level
@@ -26,6 +29,8 @@
 `define SUB {1'b1, `ALU_SUB}
 `define XOR {1'b1, `ALU_XOR}
 `define INC {1'b1, `ALU_INC}
+`define MUL {1'b1, `ALU_MUL} // Define multiplication instruction
+`define DIV {1'b1, `ALU_DIV} // Define division instruction
 // 1'b1111 NOP
 
 // --------------------------------------------------------------------
@@ -246,6 +251,22 @@ module tt_um_gak25_8bit_cpu_ext (
                 alu_in2 = 8'hxx;
                 alu_op = `ALU_INC;
             end
+	    `MUL: begin
+	        alu_in1 = r_d1;
+	        alu_in2 = r_d2;
+	        alu_op = `ALU_MUL;
+	        w_reg = r1; // destination register
+	        w_data = alu_out;
+	        write = 1'b1;
+	    end
+            `DIV: begin
+                alu_in1 = r_d1;
+                alu_in2 = r_d2;
+              	alu_op = `ALU_DIV;
+           	w_reg = r1; // destination register
+            	w_data = alu_out;
+            	write = 1'b1;
+       	    end
             default: begin
                 mux_new_data_out = 0;
                 mux_processor_stat_data_out = 0;
@@ -337,6 +358,8 @@ module alu #(
 );
 
     reg [BIT_WIDTH_REG:0] temp;
+    reg [BIT_WIDTH_REG*2:0] temp_mul; // Temporary register for multiplication results
+
 
     always @(*)
         case(op)
@@ -374,6 +397,19 @@ module alu #(
                     out = in1 + 1;
                     c = in1[7] & ~out[7];
                     temp = {BIT_WIDTH_REG+1{1'bx}};
+            `ALU_MUL: begin
+                    temp_mul = in1 * in2; // Simple multiplication
+                    out = temp_mul[BIT_WIDTH_REG-1:0]; // Assign lower bits to output
+                    c = temp_mul[BIT_WIDTH_REG*2:BIT_WIDTH_REG] != 0; // Set carry if higher bits are non-zero
+                    end
+            `ALU_DIV: begin
+                    if (in2 != 0) begin
+                	out = in1 / in2; // Division result
+                      	c = in1 % in2 != 0; // Set carry if there is a remainder
+                    end else begin
+                    	out = 0; // Division by zero case
+                    	c = 1'b1; // Set carry to indicate error
+                	end
             end
             default: begin
                         out={BIT_WIDTH_REG{1'b0}};
